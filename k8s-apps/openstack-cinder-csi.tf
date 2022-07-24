7bdb829dc46247ef0923f0b22763ab4aa250adc5
Sh4kE <michael.wittig@posteo.de>
2022-07-24 22:21:35 +0200

    add openstack cinder csi driver

---
+++ k8s-apps/openstack-cinder-csi.tf
@@ -0,0 +1,5 @@
+resource "kubernetes_manifest" "openstack-cinder-csi-argocd-application" {
+  manifest = yamldecode(file("./k8s-projects/openstack-cinder-csi/application.yaml"))
+
+  depends_on = [kubernetes_manifest.argocd-install, module.argocd-apps]
+}
