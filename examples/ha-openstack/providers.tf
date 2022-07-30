9f5dc7e57190f2ea40a02964b79bd96eca935dca
Sh4kE <michael.wittig@posteo.de>
2022-07-30 09:34:45 +0200

    add k3s vault read capabilities

--- examples/ha-openstack/providers.tf
+++ examples/ha-openstack/providers.tf
@@ -12,12 +12,5 @@ provider "kubernetes" {
   cluster_ca_certificate = data.k8sbootstrap_auth.auth.ca_crt
 }
 
-/*provider "argocd" {
-  server_addr = var.argocd_server_addr
-  username    = var.argocd_server_user
-#  password    = module.k8s-apps.argocd-initial-admin-secret.data.password
-  password    = "BWxVDwh-BwXJGrkC"
-}*/
-
 provider "vault" {
 }
--- k8s-apps/vault/main.tf
+++ k8s-apps/vault/main.tf
@@ -14,14 +14,27 @@ resource "vault_auth_backend" "kubernetes" {
   type = "kubernetes"
 }
 
-/*
 resource "vault_kubernetes_auth_backend_config" "openstack" {
   backend                = vault_auth_backend.kubernetes.path
   kubernetes_host        = "https://kubernetes.default.svc:443"
   disable_iss_validation = "true"
 }
 
-token_reviewer_jwt="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
-kubernetes_host="https://$KUBERNETES_PORT_443_TCP_ADDR:443" \
-kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-*/
+resource "vault_policy" "read_k3s_secrets" {
+  name = "read_k3s_secrets"
+
+  policy = <<EOF
+path "kv/data/k3s/argocd/*" {
+  capabilities = ["read"]
+}
+EOF
+}
+
+resource "vault_kubernetes_auth_backend_role" "argocd" {
+  backend                          = vault_auth_backend.kubernetes.path
+  role_name                        = "argocd"
+  bound_service_account_names      = ["*"]
+  bound_service_account_namespaces = ["argocd"]
+  token_ttl                        = 3600
+  token_policies                   = [vault_policy.read_k3s_secrets.name]
+}
